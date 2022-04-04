32297f2a21e6f7581a5af9b230999f15d42095cc
Juan Camilo <juankmilo0014@hotmail.com>
2022-04-03 19:30:04 -0500

    solved T-1 and T-2

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -1,8 +1,28 @@
-describe('search test case', function(){
-    it('search whit result', function(){
-        cy.visit('http://automationpractice.com/index.php');//abrir la pagina
-        cy.get('#search_query_top').type('dress');
-        cy.get('#searchbox > .btn').click();
-        cy.get('.lighter').contains('dress');
-    });
+
+describe('Search eleemnts', ()=>{
+
+    beforeEach(()=>{
+        cy.visit('/');
+    })
+
+    it('Search for element with multiple result', ()=>{
+        
+        cy.fixture('index.json').then((index) =>{
+            cy.get(index.searchBox).type("dress");
+            cy.get(index.searchButton).click();
+        })
+        cy.fixture('searchResult.json').then((searchResult)=>{
+            cy.get(searchResult.tittle).should('contain', 'dress');
+        })
+    })
+
+    it('Search for element with no result', ()=>{
+        cy.fixture('index.json').then((index) =>{
+            cy.get(index.searchBox).type("qwerty");
+            cy.get(index.searchButton).click();
+        })
+        cy.fixture('searchResult.json').then((searchResult)=>{
+            cy.get(searchResult.alert).should('contain', 'No results were found for your search');
+        })
+    })
 })
